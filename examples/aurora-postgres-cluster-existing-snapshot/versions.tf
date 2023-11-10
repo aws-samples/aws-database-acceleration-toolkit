5fc21ae5383e2662ba646cc0c436ffe15a5571fb
Mitesh Purohit <57580340+devops-arch-cloud@users.noreply.github.com>
2023-11-10 00:10:34 -0600

    Update versions.tf

--- examples/aurora-postgres-cluster-existing-snapshot/versions.tf
+++ examples/aurora-postgres-cluster-existing-snapshot/versions.tf
@@ -1,6 +1,8 @@
 terraform {
   required_version = ">= 1.0.0"
-
+  backend "local" {
+    path = "terraform_${statefile}.tfstate"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
