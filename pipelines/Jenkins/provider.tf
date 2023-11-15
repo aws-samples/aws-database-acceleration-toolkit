c940b3e6dfd7dac85d640b9d130d8d8dab95d01f
Mitesh Purohit <purohmid@amazon.com>
2023-11-14 23:37:57 -0600

    updated stages

--- pipelines/Jenkins/provider.tf
+++ pipelines/Jenkins/provider.tf
@@ -2,3 +2,12 @@ provider "aws" {
   alias  = "primary"
   region = var.region
 }
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+  }
+}
