f3a5fd8b65fbf281b6940491ad66d1a4e168c431
Mitesh Purohit <purohmid@amazon.com>
2023-11-15 19:02:57 -0600

    removed state file

--- pipelines/Jenkins/main.tf
+++ pipelines/Jenkins/main.tf
@@ -22,27 +22,3 @@ output "Jenkins_IP" {
 
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state"
- 
-  # Prevent accidental deletion of this S3 bucket
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-
-resource "aws_s3_bucket_versioning" "enabled" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket                  = aws_s3_bucket.terraform_state.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
--- pipelines/Jenkins/provider.tf
+++ pipelines/Jenkins/provider.tf
@@ -2,12 +2,3 @@ provider "aws" {
   alias  = "primary"
   region = var.region
 }
-
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-2"
-  }
-}
