b7ec99dde43ae49e9f0ec3565a274a08116a5227
mythilias <mythilias@gmail.com>
2024-08-09 16:54:59 -0500

    cleanup

--- modules/tffiles-route53/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "aws_read_endpoint_fqdn" {
-  description = "Fully qualified domain name"
-  value = "${aws_route53_record.read_endpoint.fqdn}"
-}
-
-output "aws_write_endpoint_fqdn" {
-  description = "Fully qualified domain name"
-  value = "${aws_route53_record.write_endpoint.fqdn}"
-}
