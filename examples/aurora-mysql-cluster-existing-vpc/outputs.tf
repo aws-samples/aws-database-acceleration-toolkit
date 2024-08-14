66d6d687c3e2927146e4f72bb4f6eceaf767eb2f
mythilias <mythilias@gmail.com>
2024-08-14 15:26:31 -0500

    add output variable

--- examples/aurora-mysql-cluster-existing-vpc/outputs.tf
+++ examples/aurora-mysql-cluster-existing-vpc/outputs.tf
@@ -9,11 +9,3 @@ output cluster_arn {
 output cluster_id {
      value         = module.aurora_poc.cluster_id
 }
-
-output cluster_master_password {
-     value         = nonsensitive(module.aurora_poc.cluster_master_password)
-}
-
-output cluster_master_username {
-     value         = nonsensitive(module.aurora_poc.cluster_master_username)
-}
